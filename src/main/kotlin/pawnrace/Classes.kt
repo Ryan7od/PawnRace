1c01d9411411e4e1e1f2b4d5423d2ae128a98557
ro523 <ro523@ic.ac.uk>
2023-12-05 01:21:58 +0000

    Game finished

--- src/main/kotlin/pawnrace/Classes.kt
+++ src/main/kotlin/pawnrace/Classes.kt
@@ -1,5 +1,7 @@
 package pawnrace
 
+import kotlin.math.abs
+
 enum class Piece { B, W, N;
     fun opposite() =
         when (this) {
@@ -25,10 +27,10 @@ class MutableStack<T>(var list: MutableList<T>) {
         return this
     }
 
-    fun peep(): T = list.last()
+    fun peek(): T = list.last()
 }
 
-class Game(var board: Board, var player: Piece, val moves: MutableStack<Move> = MutableStack(mutableListOf())){
+class Game(var board: Board, var player: Piece, val moves: MutableStack<Move> = MutableStack(mutableListOf())) {
     fun applyMove(move: Move) {
         board.move(move)
         player = player.opposite()
@@ -37,17 +39,121 @@ class Game(var board: Board, var player: Piece, val moves: MutableStack<Move> = 
     fun unnaplyMove() {
         val lastMove = moves.pop()
         board.board[lastMove.from.rank.rank][lastMove.from.file.file] = lastMove.piece
-        if (lastMove.type == MoveType.PEACEFUL)
+        if (lastMove.type == MoveType.PEACEFUL) {
             board.board[lastMove.to.rank.rank][lastMove.to.file.file] = Piece.N
-        if(lastMove.type == MoveType.CAPTURE)
-            board.board[lastMove.to.rank.rank][lastMove.to.file.file] = lastMove.piece.opposite()
-        else{
-            if (lastMove.piece == Piece.B)
-                board.board[lastMove.to.rank.rank+1][lastMove.to.file.file] = Piece.W
-            else
-                board.board[lastMove.to.rank.rank-1][lastMove.to.file.file] = Piece.B
         }
+        if (lastMove.type == MoveType.CAPTURE) {
+            board.board[lastMove.to.rank.rank][lastMove.to.file.file] = lastMove.piece.opposite()
+        } else {
+            if (lastMove.piece == Piece.B) {
+                board.board[lastMove.to.rank.rank + 1][lastMove.to.file.file] = Piece.W
+            } else {
+                board.board[lastMove.to.rank.rank - 1][lastMove.to.file.file] = Piece.B
+            }
+        }
+    }
 
+    fun moves(piece: Piece): List<Move> {
+        val lastMove: Move? = if (moves.isEmpty()) {
+            null
+        } else {
+            moves.peek()
+        }
+        val forward: Int = if (piece == Piece.B) {
+            -1
+        } else {
+            1
+        }
+        val list: MutableList<Move> = mutableListOf()
+        board.positionsOf(piece).forEach {
+            if (board.pieceAt(it.move(forward, 0)) == Piece.N) {
+                list.add(Move(piece, it, it.move(forward, 0), MoveType.PEACEFUL))
+            }
+            if (it.file.file < 7 && board.pieceAt(it.move(forward, 1)) == piece.opposite()) {
+                list.add(Move(piece, it, it.move(forward, 1), MoveType.CAPTURE))
+            }
+            if (it.file.file > 0 && board.pieceAt(it.move(forward, -1)) == piece.opposite()) {
+                list.add(Move(piece, it, it.move(forward, -1), MoveType.CAPTURE))
+            }
+            if (lastMove != null) {
+                if (it.file.file < 7 &&
+                    it.rank.rank < 6 && it.rank.rank > 1 &&
+                    board.pieceAt(it.move(0, 1)) == piece.opposite() &&
+                    lastMove.to == it.move(0, 1) &&
+                    lastMove.from == it.move(2 * forward, 1)
+                ) {
+                    list.add(Move(piece, it, it.move(forward, 1), MoveType.EN_PASSANT))
+                }
+                if (it.file.file > 0 &&
+                    it.rank.rank < 6 && it.rank.rank > 1 &&
+                    board.pieceAt(it.move(0, -1)) == piece.opposite() &&
+                    lastMove.to == it.move(0, -1) &&
+                    lastMove.from == it.move(2 * forward, -1)
+                ) {
+                    list.add(Move(piece, it, it.move(forward, -1), MoveType.EN_PASSANT))
+                }
+            }
+        }
+        return list.toList()
+    }
+
+    fun over(): Boolean {
+        if (board.positionsOf(Piece.B).isEmpty() ||
+            board.positionsOf(Piece.W).isEmpty()
+        ) {
+            return true
+        }
+        board.positionsOf(Piece.B).forEach { if (it.rank.rank == 0) return true }
+        board.positionsOf(Piece.W).forEach { if (it.rank.rank == 7) return true }
+        return false
+    }
+
+    fun winner(): Piece? {
+        if (board.positionsOf(Piece.B).isEmpty()) return Piece.W
+        if (board.positionsOf(Piece.W).isEmpty()) return Piece.B
+        board.positionsOf(Piece.B).forEach { if (it.rank.rank == 0) return Piece.B }
+        board.positionsOf(Piece.W).forEach { if (it.rank.rank == 7) return Piece.W }
+        return null
+    }
+
+    fun parseMove(san: String): Move? {
+        val move: Move
+        val file = san[0].uppercase()
+        val rank = san.last().digitToInt()
+        var pos: Position? = null
+        var piece: Piece = Piece.N
+        var type = MoveType.CAPTURE
+        board.positionsOf(Piece.B).forEach {
+            if (it.file.toString() == file){
+                if (abs(it.rank.rank - rank) <= 1) {
+                    pos = it
+                    piece = Piece.B
+                }
+            }
+        }
+        board.positionsOf(Piece.W).forEach {
+            if (it.file.toString() == file){
+                if (abs(it.rank.rank - rank) == 1) {
+                    pos = it
+                    piece = Piece.W
+                }
+                if (it.rank.rank == rank) {
+                    pos = it
+                    piece = Piece.W
+                    type = MoveType.EN_PASSANT
+                }
+            }
+        }
+        if(pos == null) {
+            return null
+        }
+        val pos2 = Position(file + rank.toString())
+        if (san.length == 2) {
+            move = Move(piece, pos!!, pos2, MoveType.PEACEFUL)
+        } else {
+            move = Move(piece, pos!!, pos2, type)
+        }
+        return move
     }
 }
 
@@ -180,11 +286,12 @@ class Board(val whiteGap: File, val blackGap: File) {
         if (isValidMove(m)) {
             board[m.to.rank.rank][m.to.file.file] = board[m.from.rank.rank][m.from.file.file]
             board[m.from.rank.rank][m.from.file.file] = Piece.N
-            if(m.type == MoveType.EN_PASSANT){
-                if (m.piece == Piece.B)
-                    board[m.to.rank.rank+1][m.to.file.file] = Piece.N
-                else
-                    board[m.to.rank.rank-1][m.to.file.file] = Piece.N
+            if (m.type == MoveType.EN_PASSANT) {
+                if (m.piece == Piece.B) {
+                    board[m.to.rank.rank + 1][m.to.file.file] = Piece.N
+                } else {
+                    board[m.to.rank.rank - 1][m.to.file.file] = Piece.N
+                }
             }
         }
     }
